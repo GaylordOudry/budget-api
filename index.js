b2b994c4fdab279a8a4f1f741a5df360c02643d9
GaylordOudry <103671263+GaylordOudry@users.noreply.github.com>
2022-04-19 19:13:18 +0200

    fix some bugs with resolvers

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import Express         from "express";
-import { graphqlHTTP } from "express-graphql";
-import cors            from "cors";
-import { config }      from "dotenv";
-import mongoose        from "mongoose";
-import {Schema}        from "./Schema.js";
-import {Resolvers}     from "./Resolvers.js";
+import Express                from "express";
+import { graphqlHTTP }        from "express-graphql";
+import cors                   from "cors";
+import { config }             from "dotenv";
+import mongoose               from "mongoose";
+import {resolvers}            from "./Resolvers.js";
+import { readFileSync }       from "fs";
+import {makeExecutableSchema} from "@graphql-tools/schema";
 config();
 const App = Express();
 const PORT = process.env.PORT
 const DB_URL = process.env.DB_URL
+
+const typeDefs = readFileSync("./Schema.graphql", "utf-8");
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 mongoose.connect(DB_URL).then(() => {
 	App.listen(PORT, () => {
@@ -18,7 +23,7 @@ mongoose.connect(DB_URL).then(() => {
 
 App.use(cors());
 App.use(Express.json())
-App.use("/graphql", graphqlHTTP({ schema: Schema, rootValue: Resolvers, graphiql: true, pretty: true }))
+App.use("/graphql", graphqlHTTP({ schema, graphiql: true, pretty: true }))
 App.get("/", (_, res) => {
 	//res.send("Hello")
 	res.redirect('/graphql')
